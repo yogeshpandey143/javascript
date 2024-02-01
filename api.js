1bae2984a4cccea49b8b80fb671329cdffdb3717
Yogesh Pandey <bc21143@smsvaranasi.in>
2024-02-01 18:40:53 +0530

    api calling

---
+++ api.js
@@ -0,0 +1,24 @@
+const URL="https://cat-fact.herokuapp.com/facts";
+let factPara= document.querySelector(".fact");
+let btn= document.querySelector(".btn");
+//let proise = fetch(url,[option])
+
+
+const getFact= async ()=>{
+    console.log("getting data---")
+    let promise=await fetch(URL);
+    console.log(promise.status);
+    console.log(promise);
+
+    // JSON format-> use json()
+    let data= await promise.json();
+    console.log(data[0].text);
+    factPara.innerText=data[2].text;
+
+}
+
+btn.addEventListener("click",getFact);
+
+//AJAX is Asynchronous JS And  XML
+
+//JSON is javascript Object Notation 
